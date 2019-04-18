9b1c6522630ee54bc6d78df69f0048186b90e226
gleim@em.uni-frankfurt.de <MbJfYHcNJKmBZx1V47pX>
2019-04-18 08:03:16 +0200

    Added CassandraWriter

--- src/main/java/org/hucompute/wikidragon/core/nlp/textimager/WikiDragonCassandraCollectionReader.java
+++ src/main/java/org/hucompute/wikidragon/core/nlp/textimager/WikiDragonCassandraCollectionReader.java
@@ -2,15 +2,12 @@ package org.hucompute.wikidragon.core.nlp.textimager;
 
 import com.datastax.driver.core.*;
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
-
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.uima.UimaContext;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.cas.impl.XmiCasDeserializer;
-import org.apache.uima.cas.impl.XmiCasSerializer;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.fit.component.CasCollectionReader_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
@@ -18,14 +15,15 @@ import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.Progress;
 import org.xml.sax.SAXException;
-
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.*;
 
-public class WikiDragonCassandraCollectionReader extends CasCollectionReader_ImplBase implements AutoCloseable, Progress {
+public class WikiDragonCassandraCollectionReader extends CasCollectionReader_ImplBase implements Progress {
 
     private static Logger logger = LogManager.getLogger(WikiDragonCassandraCollectionReader.class);
+
+    public enum ProcessingState {PROCESSED, UNPROCESSED, ANY};
 
     public static final String PARAM_KEYSPACE = "keyspace";
     @ConfigurationParameter(name=PARAM_KEYSPACE, mandatory=true)
@@ -47,42 +45,35 @@ public class WikiDragonCassandraCollectionReader extends CasCollectionReader_Imp
     @ConfigurationParameter(name=PARAM_CONTACTHOSTS, mandatory=true)
     private String[] contactHosts;
 
-
     public static final String PARAM_PROCESSINGSTATE = "processingState";
     @ConfigurationParameter(name=PARAM_PROCESSINGSTATE, mandatory=true)
-    private TextImagerCassandraDriver.ProcessingState processingState;
+    private ProcessingState processingState;
 
     public static final String PARAM_SKIPZEROLENGTH = "skipZeroLength";
     @ConfigurationParameter(name=PARAM_SKIPZEROLENGTH, mandatory=true)
     private boolean skipZeroLength;
-    
-    public static final String PARAM_POOLDOCUMENTS = "poolDocuments";
-    @ConfigurationParameter(name=PARAM_POOLDOCUMENTS, mandatory=true)
-    private boolean poolDocuments;
-    
-    public static final String PARAM_POOLDOCUMENTSMAXBYTES = "poolDocumentsMaxBytes";
-    @ConfigurationParameter(name=PARAM_POOLDOCUMENTSMAXBYTES, mandatory=true)
-    private int poolDocumentsMaxBytes;
 
     private ResultSet resultSet;
     private String[] next = null;
-    private int pooledDocumentsRead = 0;
-    private int pooledDocumentsRelevant = 0;
-    private int documentsRead = 0;
     private String language;
     private Cluster cluster;
     private Session session;
+    private long documentsRead;
+    private long relevantDocumentsRead;
+    private long prefetchDocumentsRead;
+    private long prefetchRelevantDocumentsRead;
+    private long documentsTotal;
+    private long relevantDocumentsTotal;
     private boolean currentlyInComputePooledDocumentsRelevant = false;
     
     @Override
     public void initialize(UimaContext aContext) throws ResourceInitializationException {
-    	// TODO Auto-generated method stub
     	super.initialize(aContext);
         try {
 			init();
-		} catch (CollectionException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+        catch (CollectionException | IOException e) {
+			throw new ResourceInitializationException(e);
 		}
     }
     
@@ -92,53 +83,42 @@ public class WikiDragonCassandraCollectionReader extends CasCollectionReader_Imp
             lBuilder.addContactPoint(lHost);
         }
         lBuilder.withCredentials(user, password);
-        lBuilder.getConfiguration().getQueryOptions().setConsistencyLevel(TextImagerCassandraDriver.defaultWriteConsistencyLevel);
+        lBuilder.getConfiguration().getQueryOptions().setConsistencyLevel(ConsistencyLevel.ANY);
         lBuilder.getConfiguration().getSocketOptions().setConnectTimeoutMillis(30000); // Default: 5000
         lBuilder.getConfiguration().getSocketOptions().setReadTimeoutMillis(30000); // Default 12000
         cluster = lBuilder.build();
         session = cluster.connect();
-        pooledDocumentsRelevant = computePooledDocumentsRelevant();
+        documentsRead = 0;
+        relevantDocumentsRead = 0;
+        prefetchDocumentsRead = 0;
+        prefetchRelevantDocumentsRead = 0;
+        computePooledDocumentsRelevant();
         resultSet = session.execute("select dbname,raw,textlen,xmilen,processed,xmi from wikitextspannlp");
+        documentsRead = 0;
+        relevantDocumentsRead = 0;
+        prefetchDocumentsRead = 0;
+        prefetchRelevantDocumentsRead = 0;
         prefetch();
     }
 
-    public WikiDragonCassandraCollectionReader(){
-    }
-    
-    public WikiDragonCassandraCollectionReader(String pKeyspace, String pUser, String pPassword, String[] pContactHosts, String pDBName, TextImagerCassandraDriver.ProcessingState pProcessingState, boolean pSkipZeroLength, boolean pPoolDocuments, int pPoolDocumentsMaxBytes) throws IOException, CollectionException {
-        keyspace = pKeyspace;
-        user = pUser;
-        password = pPassword;
-        dbname = pDBName;
-        contactHosts = pContactHosts;
-        processingState = pProcessingState;
-        skipZeroLength = pSkipZeroLength;
-        poolDocuments = pPoolDocuments;
-        poolDocumentsMaxBytes = pPoolDocumentsMaxBytes;
-        init();
-    }
-
-    private int computePooledDocumentsRelevant() throws IOException, CollectionException {
+    private void computePooledDocumentsRelevant() throws IOException, CollectionException {
         currentlyInComputePooledDocumentsRelevant = true;
-        int lResult = 0;
         session.execute("use "+keyspace);
         resultSet = session.execute("select dbname,raw,textlen,xmilen,processed from wikitextspannlp");
         prefetch();
         long lLastTime = System.currentTimeMillis();
+        documentsTotal = 0;
+        relevantDocumentsTotal = 0;
         while (hasNext()) {
             getNext(null);
-            lResult++;
+            relevantDocumentsTotal++;
             if (System.currentTimeMillis() - lLastTime >= 1000) {
                 lLastTime = System.currentTimeMillis();
-                logger.info("Atomic Docs Read: "+documentsRead+", RelevantPooledDocs: "+lResult);
+                logger.info("Documents Read: "+documentsTotal+", Documents Relevant: "+relevantDocumentsTotal);
             }
         }
-        logger.info("Atomic Docs Read: "+documentsRead+", RelevantPooledDocs: "+pooledDocumentsRead);
-        pooledDocumentsRead = 0;
-        pooledDocumentsRelevant = 0;
-        documentsRead = 0;
+        logger.info("Documents Read: "+documentsTotal+", Documents Relevant: "+relevantDocumentsTotal);
         currentlyInComputePooledDocumentsRelevant = false;
-        return lResult;
     }
 
     private boolean accept(Row pRow) {
@@ -186,25 +166,10 @@ public class WikiDragonCassandraCollectionReader extends CasCollectionReader_Imp
                 }
                 lFoundOne = true;
                 next = new String[]{lKey, lXMI, lXMILength};
+                prefetchRelevantDocumentsRead++;
             }
-            documentsRead++;
+            prefetchDocumentsRead++;
         }
-    }
-
-    /**
-     * Get number of pooled documents which have been newly added in this run
-     * @return
-     */
-    public int getPooledDocumentsRead() {
-        return pooledDocumentsRead;
-    }
-
-    /**
-     * Get number of (atomic) documents which have been read from the database, whether they have to be considered or not
-     * @return
-     */
-    public int getDocumentsRead() {
-        return documentsRead;
     }
 
     @Override
@@ -236,31 +201,11 @@ public class WikiDragonCassandraCollectionReader extends CasCollectionReader_Imp
 
     @Override
     public void getNext(CAS cas) throws IOException, CollectionException {
+        documentsRead = prefetchDocumentsRead;
+        relevantDocumentsRead = prefetchRelevantDocumentsRead;
         if (next != null) {
-            byte[] lBytes = null;
-            if (poolDocuments) {
-                List<FastDocument> lDocuments = new ArrayList<>();
-                int lBytesLength = Integer.parseInt(next[2]);
-                if (next[1] != null) lDocuments.add(FastDocument.fromXMI(next[1]));
-                prefetch();
-                pooledDocumentsRead++;
-                while ((lBytesLength <= poolDocumentsMaxBytes) && (next != null)) {
-                    if (lBytesLength + Integer.parseInt(next[2]) <= poolDocumentsMaxBytes) {
-                        lBytesLength += Integer.parseInt(next[2]);
-                        if (next[1] != null) lDocuments.add(FastDocument.fromXMI(next[1]));
-                        prefetch();
-                    }
-                    else {
-                        break;
-                    }
-                }
-                lBytes = lDocuments.size() > 0 ? join(lDocuments).exportXMI().getBytes(Charset.forName("UTF-8")) : null;
-            }
-            else {
-                lBytes = next[1] != null ? next[1].getBytes(Charset.forName("UTF-8")) : null;
-                prefetch();
-                pooledDocumentsRead++;
-            }
+            byte[] lBytes = next[1] != null ? next[1].getBytes(Charset.forName("UTF-8")) : null;
+            prefetch();
             if (cas != null) {
                 try {
                     XmiCasDeserializer.deserialize(new ByteArrayInputStream(lBytes), cas, false);
@@ -272,16 +217,16 @@ public class WikiDragonCassandraCollectionReader extends CasCollectionReader_Imp
                     else if (lDocumentMetaDataCollection.size() == 1) {
                         DocumentMetaData lDocumentMetaData = lDocumentMetaDataCollection.iterator().next();
                         if ((lDocumentMetaData.getDocumentTitle() == null) || (lDocumentMetaData.getDocumentTitle().length() == 0)) {
-                            lDocumentMetaData.setDocumentTitle(pooledDocumentsRead + ".xmi");
+                            lDocumentMetaData.setDocumentTitle(documentsRead + ".xmi");
                         }
                         if ((lDocumentMetaData.getDocumentId() == null) || (lDocumentMetaData.getDocumentId().length() == 0)) {
-                            lDocumentMetaData.setDocumentId(pooledDocumentsRead + ".xmi");
+                            lDocumentMetaData.setDocumentId(documentsRead + ".xmi");
                         }
                     }
                     else {
                         DocumentMetaData lDocumentMetaData = new DocumentMetaData(cas.getJCas(), 0, cas.getDocumentText().length());
-                        lDocumentMetaData.setDocumentTitle(pooledDocumentsRead + ".xmi");
-                        lDocumentMetaData.setDocumentId(pooledDocumentsRead + ".xmi");
+                        lDocumentMetaData.setDocumentTitle(documentsRead + ".xmi");
+                        lDocumentMetaData.setDocumentId(documentsRead + ".xmi");
                     }
                 }
                 catch (CASException e) {
@@ -315,12 +260,12 @@ public class WikiDragonCassandraCollectionReader extends CasCollectionReader_Imp
 
     @Override
     public long getCompleted() {
-        return pooledDocumentsRead;
+        return relevantDocumentsRead;
     }
 
     @Override
     public long getTotal() {
-        return pooledDocumentsRelevant;
+        return relevantDocumentsTotal;
     }
 
     @Override
@@ -333,35 +278,4 @@ public class WikiDragonCassandraCollectionReader extends CasCollectionReader_Imp
         return false;
     }
 
-    private static FastDocument join(List<FastDocument> pFastDocuments) throws IOException {
-        StringBuilder lJointText = new StringBuilder();
-        FastDocument lResult = new FastDocument("");
-        int lOffset = 0;
-        int lIndexOrderOffset = 0;
-        for (FastDocument lFastDocument:pFastDocuments) {
-            int lCurrentMaxOrderOffset = 0;
-            if (lFastDocument == null) return null;
-            lResult.setLanguage(lFastDocument.getLanguage());
-            lJointText.append(lFastDocument.getText());
-            for (FastAnnotation lAnnotation:lFastDocument.getAnnotations(true)) {
-                FastAnnotation lNewAnnotation = lResult.addAnnotation(lAnnotation.getTypeUri(), lAnnotation.getName(), lAnnotation.getBegin() + lOffset, lAnnotation.getEnd() + lOffset);
-                for (Map.Entry<String, String> lEntry:lAnnotation.getAttributes().entrySet()) {
-                    lNewAnnotation.setAttributeValue(lEntry.getKey(), lEntry.getValue());
-                }
-                // Fix Order-Attribute of HtmlTag
-                if (lNewAnnotation.getTypeUri().equals(FastDocument.NS_WIKIDRAGON_URI) && (lNewAnnotation.getName().equals("HtmlTag"))) {
-                    int lOrder = Integer.parseInt(lNewAnnotation.getAttributeValue("order", null));
-                    if (lOrder > lCurrentMaxOrderOffset) {
-                        lCurrentMaxOrderOffset = lOrder;
-                    }
-                    lNewAnnotation.setAttributeValue("order", Integer.toString(lOrder+lIndexOrderOffset));
-                }
-            }
-            lJointText.append(' '); // Add a dummy whitespace for better readability.
-            lOffset = lJointText.length();
-            lIndexOrderOffset += lCurrentMaxOrderOffset+1;
-        }
-        lResult.setText(lJointText.toString());
-        return lResult;
-    }
 }
--- src/main/java/org/hucompute/wikidragon/core/nlp/textimager/WikiDragonCassandraWriter.java
+++ src/main/java/org/hucompute/wikidragon/core/nlp/textimager/WikiDragonCassandraWriter.java
@@ -5,6 +5,7 @@ import java.nio.charset.Charset;
 import java.util.*;
 
 import com.datastax.driver.core.*;
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.uima.UimaContext;
@@ -14,13 +15,15 @@ import org.apache.uima.cas.impl.XmiCasSerializer;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.fit.component.JCasConsumer_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
 import org.apache.uima.util.XMLSerializer;
+import org.hucompute.wikidragon.core.nlp.annotation.WikiTextSpan;
 import org.xml.sax.SAXException;
 
-public class WikiDragonCassandraWriter extends JCasConsumer_ImplBase implements AutoCloseable {
+public class WikiDragonCassandraWriter extends JCasConsumer_ImplBase {
 
     private static Logger logger = LogManager.getLogger(WikiDragonCassandraCollectionReader.class);
 
@@ -49,24 +52,13 @@ public class WikiDragonCassandraWriter extends JCasConsumer_ImplBase implements 
     private PreparedStatement preparedStatement;
     private long written = 0;
 
-    public WikiDragonCassandraWriter() {
-    }
-
-    public WikiDragonCassandraWriter(String pKeyspace, String pUser, String pPassword, String[] pContactHosts, String pDBName) throws IOException, CollectionException {
-        keyspace = pKeyspace;
-        user = pUser;
-        password = pPassword;
-        dbname = pDBName;
-        contactHosts = pContactHosts;
-    }
-
     private void init() throws CollectionException, IOException {
         Cluster.Builder lBuilder = Cluster.builder();
         for (String lHost:contactHosts) {
             lBuilder.addContactPoint(lHost);
         }
         lBuilder.withCredentials(user, password);
-        lBuilder.getConfiguration().getQueryOptions().setConsistencyLevel(TextImagerCassandraDriver.defaultWriteConsistencyLevel);
+        lBuilder.getConfiguration().getQueryOptions().setConsistencyLevel(ConsistencyLevel.ANY);
         lBuilder.getConfiguration().getSocketOptions().setConnectTimeoutMillis(30000); // Default: 5000
         lBuilder.getConfiguration().getSocketOptions().setReadTimeoutMillis(30000); // Default 12000
         cluster = lBuilder.build();
@@ -93,66 +85,48 @@ public class WikiDragonCassandraWriter extends JCasConsumer_ImplBase implements 
     @Override
     public void batchProcessComplete() throws AnalysisEngineProcessException {
         super.batchProcessComplete();
-        try {
-            close();
-        }
-        catch (IOException e) {
-            throw new AnalysisEngineProcessException(e);
-        }
+        close();
     }
 
     @Override
     public void collectionProcessComplete() throws AnalysisEngineProcessException {
         super.collectionProcessComplete();
-        try {
-            close();
-        }
-        catch (IOException e) {
-            throw new AnalysisEngineProcessException(e);
-        }
+        close();
     }
 
     @Override
     public void process(JCas jCas) throws AnalysisEngineProcessException {
-        ByteArrayOutputStream lOutput = new ByteArrayOutputStream();
-        try {
-            XmiCasSerializer.serialize(jCas.getCas(), lOutput);
+        Collection<WikiTextSpan> lDocumentMetaDataCollection = JCasUtil.select(jCas, WikiTextSpan.class);
+        if (lDocumentMetaDataCollection.size() == 0) {
+            logger.warn("Received JCas without a WikiTextSpan - Ignoring it");
         }
-        catch(SAXException e) {
-            throw new AnalysisEngineProcessException(e);
+        else if (lDocumentMetaDataCollection.size() > 1) {
+            logger.warn("Received JCas with multiple WikiTextSpans - Ignoring it");
         }
-        String lRawXMI = new String(lOutput.toByteArray());
-        try {
-            for (FastDocument lDocument : split(FastDocument.fromXMI(lRawXMI))) {
-                List<FastAnnotation> lAnnotations = lDocument.getAnnotations(FastDocument.NS_WIKIDRAGON_URI, "WikiTextSpan", false);
-                if (lAnnotations.size() == 1) {
-                    String lXMI = lDocument.exportXMI();
-                    String lUID = lAnnotations.get(0).getAttributeValue("uid", null);
-                    ResultSet lResultSet = session.execute(preparedStatement.bind(lXMI, lXMI.getBytes(Charset.forName("UTF-8")).length, dbname, lUID));
-                    if (!lResultSet.wasApplied()) {
-                        throw new AnalysisEngineProcessException(new IOException("Update could not be applied"));
-                    }
-                }
-                logger.info("Written Documents: "+(++written));
+        else {
+            String lUID = lDocumentMetaDataCollection.iterator().next().getUid();
+            ByteArrayOutputStream lOutput = new ByteArrayOutputStream();
+            try {
+                XmiCasSerializer.serialize(jCas.getCas(), lOutput);
             }
-        }
-        catch (IOException e) {
-            throw new AnalysisEngineProcessException(e);
+            catch(SAXException e) {
+                throw new AnalysisEngineProcessException(e);
+            }
+            String lXMI = new String(lOutput.toByteArray());
+            ResultSet lResultSet = session.execute(preparedStatement.bind(lXMI, lXMI.getBytes(Charset.forName("UTF-8")).length, dbname, lUID));
+            if (!lResultSet.wasApplied()) {
+                throw new AnalysisEngineProcessException(new IOException("Update could not be applied"));
+            }
+            logger.info("Written Documents: "+(++written));
         }
     }
 
     @Override
     public void destroy() {
-        try {
-            close();
-        }
-        catch (IOException e) {
-            logger.error(e.getMessage(), e);
-        }
+        close();
     }
 
-    @Override
-    public void close() throws IOException {
+    public void close() {
         if (session != null) {
             logger.info("Closing Cassandra Session of WikiDragonCassandraWriter...");
             session.close();
@@ -165,43 +139,6 @@ public class WikiDragonCassandraWriter extends JCasConsumer_ImplBase implements 
             cluster = null;
             logger.info("Closing Cassandra Cluster of WikiDragonCassandraWriter... Done");
         }
-    }
-
-    public static List<FastDocument> split(FastDocument pFastDocument) throws AnalysisEngineProcessException {
-        List<FastDocument> lResult = new ArrayList<>();
-        for (FastAnnotation lWikiTextSpan:pFastDocument.getAnnotations(FastDocument.NS_WIKIDRAGON_URI, "WikiTextSpan", true)) {
-            int lOffset = lWikiTextSpan.getBegin();
-            FastDocument lNewDocument = new FastDocument(pFastDocument.getLanguage(), lWikiTextSpan.toString());
-            List<FastAnnotation> lAnnotations = pFastDocument.getSubsumedAnnotations(lWikiTextSpan.getBegin(), lWikiTextSpan.getEnd(), true);
-            int lMinOrder = Integer.MAX_VALUE;
-            for (FastAnnotation lAnnotation:lAnnotations) {
-                if (lAnnotation.getTypeUri().equals(FastDocument.NS_WIKIDRAGON_URI) && lAnnotation.getName().equals("HtmlTag")) {
-                    int lOrder = Integer.parseInt(lAnnotation.getAttributeValue("order", null));
-                    lMinOrder = Math.min(lMinOrder, lOrder);
-                }
-            }
-            if (lMinOrder == Integer.MAX_VALUE) lMinOrder = 0;
-            for (FastAnnotation lAnnotation:lAnnotations) {
-                FastAnnotation lNewAnnotation = lNewDocument.addAnnotation(lAnnotation.getTypeUri(), lAnnotation.getName(), lAnnotation.getBegin()-lOffset, lAnnotation.getEnd()-lOffset);
-                if (lAnnotation.getTypeUri().equals(FastDocument.NS_WIKIDRAGON_URI) && lAnnotation.getName().equals("HtmlTag")) {
-                    for (Map.Entry<String, String> lEntry:lAnnotation.getAttributes().entrySet()) {
-                        if (lEntry.getKey().equals("order")) {
-                            lNewAnnotation.setAttributeValue(lEntry.getKey(), Integer.toString(Integer.parseInt(lEntry.getValue())-lMinOrder));
-                        }
-                        else {
-                            lNewAnnotation.setAttributeValue(lEntry.getKey(), lEntry.getValue());
-                        }
-                    }
-                }
-                else {
-                    for (Map.Entry<String, String> lEntry:lAnnotation.getAttributes().entrySet()) {
-                        lNewAnnotation.setAttributeValue(lEntry.getKey(), lEntry.getValue());
-                    }
-                }
-            }
-            lResult.add(lNewDocument);
-        }
-        return lResult;
     }
 
 }
